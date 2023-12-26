b68ed19f17525724353b5612b0ff7aaa3ba59393
mertyldrr <mertyildirir14@gmail.com>
2023-12-26 21:38:13 +0100

    add featured blog post component

---
+++ src/app/featured-blog-post/featured-blog-post.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FeaturedBlogPostComponent } from './featured-blog-post.component';
+
+describe('FeaturedBlogPostComponent', () => {
+  let component: FeaturedBlogPostComponent;
+  let fixture: ComponentFixture<FeaturedBlogPostComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FeaturedBlogPostComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FeaturedBlogPostComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/featured-blog-post/featured-blog-post.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-featured-blog-post',
+  standalone: true,
+  imports: [],
+  templateUrl: './featured-blog-post.component.html',
+  styleUrl: './featured-blog-post.component.css'
+})
+export class FeaturedBlogPostComponent {
+
+}
