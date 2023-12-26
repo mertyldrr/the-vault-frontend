da8ee1bfd4fa15e79abe89c392b58931677678db
mertyldrr <mertyildirir14@gmail.com>
2023-12-26 21:32:59 +0100

    add fonts and fa icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        newsletter: "url('src/assets/newsletter-bg.jpeg')",
+      },
+      fontFamily: {
+        notoDisplay: ['Noto Sans Display Variable', 'sans-serif'],
+        notoSerif: [
+          'Noto Serif Variable',
+          'Times New Roman',
+          'Georgia',
+          'serif',
+        ],
+        atkinson: ['Atkinson Hyperlegible', 'sans-serif'],
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require('daisyui')],
+};
