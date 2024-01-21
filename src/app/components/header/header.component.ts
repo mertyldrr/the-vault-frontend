75b44c40617b1b7ba4647ff6d8e69c6ff3089efc
mertyldrr <mertyildirir14@gmail.com>
2024-01-21 17:23:03 +0100

    update style of header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { faMoon, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun, faUser } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ThemeService } from '../../theme/theme.service';
+import { ModalService } from '../../services/modal/modal.service';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +12,24 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-  protected readonly faMoon = faMoon;
-  protected readonly faUser = faUser;
+  public readonly faMoon = faMoon;
+  public readonly faUser = faUser;
+  public readonly faSun = faSun;
+
+  constructor(
+    private modalService: ModalService,
+    protected themeService: ThemeService
+  ) {}
+
+  showSignupModal(): void {
+    this.modalService.showSignUpModal();
+  }
+
+  showLoginModal(): void {
+    this.modalService.showLoginModal();
+  }
+
+  toggleTheme() {
+    this.themeService.toggleTheme();
+  }
 }
