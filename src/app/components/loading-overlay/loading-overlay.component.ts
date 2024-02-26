a4ae5b7de7ddab9a96adf5e7823c6e928fe8183a
mertyldrr <mertyildirir14@gmail.com>
2024-02-26 22:06:26 +0100

    add loading overlay

---
+++ src/app/components/loading-overlay/loading-overlay.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LoadingSpinnerComponent } from '../loading-spinner/loading-spinner.component';
+import { NgIf } from '@angular/common';
+
+@Component({
+  selector: 'app-loading-overlay',
+  standalone: true,
+  imports: [LoadingSpinnerComponent, NgIf],
+  templateUrl: './loading-overlay.component.html',
+  styleUrl: './loading-overlay.component.css',
+})
+export class LoadingOverlayComponent {}
