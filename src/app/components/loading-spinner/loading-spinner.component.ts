10bec992441de61d07a082e85cbbc03411bc41c0
mertyldrr <mertyildirir14@gmail.com>
2024-03-02 07:33:00 +0100

    feat(loading): add loading overlay and update loading spinner

--- src/app/components/loading-spinner/loading-spinner.component.ts
+++ src/app/components/loading-spinner/loading-spinner.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { clsx } from 'clsx';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-loading-spinner',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './loading-spinner.component.html',
-  styleUrl: './loading-spinner.component.css',
+  styleUrls: ['./loading-spinner.component.css'], // Fix the typo here
 })
-export class LoadingSpinnerComponent {}
+export class LoadingSpinnerComponent {
+  @Input() size: 'sm' | 'lg' = 'sm';
+  @Input() twColor: string | undefined = 'border-blue-400';
+  getFixedCircleClass(): string {
+    return clsx(
+      'border-gray rounded-full',
+      { 'w-6 h-6 border-t-2 border-b-2': this.size === 'sm' },
+      { 'h-24 w-24 border-t-8 border-b-8': this.size === 'lg' }
+    );
+  }
+
+  getSpinCircleClasses(): string {
+    return clsx(
+      `absolute top-0 left-0 rounded-full ${this.twColor} animate-spin`,
+      { 'w-6 h-6 border-t-2 border-b-2': this.size === 'sm' },
+      { 'h-24 w-24 border-t-8 border-b-8': this.size === 'lg' }
+    );
+  }
+}
