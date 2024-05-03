e7ee1987190ec3121dec8f0f5a31330dd3ad07e5
mertyldrr <mertyildirir14@gmail.com>
2024-05-03 21:15:15 +0200

    feat(my-posts): sort blog posts based on lastUpdated date

--- src/app/pages/my-posts/my-posts-page.component.ts
+++ src/app/pages/my-posts/my-posts-page.component.ts
@@ -1,14 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { BlogPostService } from '../../services/blog-post/blog-post.service';
 import { BlogPost } from '../../models/blog-post.interface';
-import {
-  catchError,
-  filter,
-  Observable,
-  Subject,
-  switchMap,
-  takeUntil,
-} from 'rxjs';
+import { catchError, filter, map, Observable, Subject, switchMap, takeUntil } from 'rxjs';
 import { BlogPostCardComponent } from '../../components/blog-post-card/blog-post-card.component';
 import { UserService } from '../../services/user/user.service';
 import { NgForOf } from '@angular/common';
@@ -18,12 +11,7 @@ import { BlogPostCardListComponent } from '../../components/blog-post-card-list/
 @Component({
   selector: 'app-my-posts',
   standalone: true,
-  imports: [
-    BlogPostCardComponent,
-    NgForOf,
-    BlogPostPage,
-    BlogPostCardListComponent,
-  ],
+  imports: [BlogPostCardComponent, NgForOf, BlogPostPage, BlogPostCardListComponent],
   templateUrl: './my-posts-page.component.html',
   styleUrl: './my-posts-page.component.css',
 })
@@ -40,6 +28,7 @@ export class MyPostsPage implements OnInit, OnDestroy {
       takeUntil(this.destroy$),
       filter(user => !!user), // Filter out null user
       switchMap(user => this.blogPostService.getBlogPostsByUserId(user!.id)),
+      map(blogPosts => this.sortBlogPosts(blogPosts)),
       catchError(error => {
         console.error('Error fetching blog posts:', error);
         // Handle the error, return an empty array or re-throw the error
@@ -51,5 +40,13 @@ export class MyPostsPage implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  private sortBlogPosts(blogPosts: BlogPost[]): BlogPost[] {
+    return blogPosts.sort((a, b) => {
+      const dateA = new Date(a.updatedAt).getTime();
+      const dateB = new Date(b.updatedAt).getTime();
+      return dateB - dateA; // Sort in descending order, latest first
+    });
   }
 }
